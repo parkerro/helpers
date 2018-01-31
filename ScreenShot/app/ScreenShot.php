2956d6df672a61fda49b39852372cfc3311e7a97
parker <parher@staff.pchome.com.tw>
2018-01-31 16:43:52 +0800

    第一次提交 新增 tools -> ScreenShot

---
+++ ScreenShot/app/ScreenShot.php
@@ -0,0 +1,28 @@
+<?
+/**
+*   @author ParkerRo
+*   使用google api 取得網頁縮圖
+*   $ScreenShot = new ScreenShot;
+*   $ScreenShot->get_img('https://parkerro.tw/');
+*/
+class ScreenShot{
+
+    public function get_img($site_url = '')
+    {
+    	$google_api = "https://www.googleapis.com/pagespeedonline/v1/runPagespeed?url=".$site_url."&screenshot=true";
+
+    	$return = file_get_contents($google_api);
+    	$return = json_decode($return, true);
+
+        if($return['error']){
+            echo "Api error.";
+            exit;
+        }
+        $screenshot = $return['screenshot']['data'];
+        $screenshot = str_replace(array('_','-'),array('/','+'),$screenshot); 
+
+        echo "<img src=\"data:image/jpeg;base64,".$screenshot."\" />";
+    }
+}
+
+
