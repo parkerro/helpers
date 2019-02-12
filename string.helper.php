8651336f71f4e2e236867ba4a95aa3542edf10bc
parker <parher@staff.pchome.com.tw>
2019-02-12 09:18:29 +0800

    string helper Add removeEmojis function

--- string.helper.php
+++ string.helper.php
@@ -5,6 +5,9 @@
 */
 class stringHelper{
 
+    /**
+     * SQL 安全性基本過濾
+     */
     public static function SQLReplace($string) {
         //$string = strtoupper($string);
 
@@ -25,7 +28,38 @@ class stringHelper{
         return str_replace($injectionWords, '', $string);
     }
 
+    /**
+     * 內文 URL 直接轉換成可點擊的 <a> tags
+     */
     public static function makeClickableLinks($url) {
         return preg_replace('@(https?://([-\w\.]+[-\w])+(:\d+)?(/([\w/_\.#-]*(\?\S+)?[^\.\s])?)?)@', '<a href="$1" target="_blank">$1</a>', $url);
+    }
+
+    /**
+     * emoji 去除工具
+     */
+    public static function removeEmojis($string) {
+
+        // Match Emoticons
+        $regex_emoticons = '/[\x{1F600}-\x{1F64F}]/u';
+        $clear_string = preg_replace($regex_emoticons, '', $string);
+
+        // Match Miscellaneous Symbols and Pictographs
+        $regex_symbols = '/[\x{1F300}-\x{1F5FF}]/u';
+        $clear_string = preg_replace($regex_symbols, '', $clear_string);
+
+        // Match Transport And Map Symbols
+        $regex_transport = '/[\x{1F680}-\x{1F6FF}]/u';
+        $clear_string = preg_replace($regex_transport, '', $clear_string);
+
+        // Match Miscellaneous Symbols
+        $regex_misc = '/[\x{2600}-\x{26FF}]/u';
+        $clear_string = preg_replace($regex_misc, '', $clear_string);
+
+        // Match Dingbats
+        $regex_dingbats = '/[\x{2700}-\x{27BF}]/u';
+        $clear_string = preg_replace($regex_dingbats, '', $clear_string);
+
+        return $clear_string;
     }
 }
